cc8baf77f3c6b50e00a8e3130d42d7e8f8e18259
waeeelhajji <waelhajji.contact@gmail.com>
2024-11-29 11:58:46 +0100

    Week1Day5

---
+++ Algos/W1D5/recursion.js
@@ -0,0 +1,26 @@
+// recursion?
+
+// 5! 5*4*3*2*1 
+// 3! 3*2*1
+// 1! 1
+// 0! 1
+// 4! 4*3*2*1
+
+function factorial(num) {
+  // 1. BASE CASE / EXIT CONDITION
+  // Base case: factorial of 0 or 1 is 1
+  // 2. Recursive logic: The function calls itself, reducing num by 1 each time until the base case is reached.
+}
+
+console.log(factorial(4));
+
+
+
+
+
+function power(x, n) {
+  // Base case: Any number raised to the power of 0 is 1
+  // Recursive logic: The function calls itself, reducing n by 1 each time until the base case is reached.
+}
+
+console.log(power(2, 3)); 
